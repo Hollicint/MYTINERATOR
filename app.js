fc1c562fad30d86d817559c0932eeb7231ac712f
Shaneburk3 <92030458+Shaneburk3@users.noreply.github.com>
2024-05-17 15:56:05 +0100

    Change from html to ejs
    Updated file with ejs style format.

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const express = require("express");
 //create the Express app
 const app = express();
+
+const path = require('path');
+
 //listen for incoming requests
 app.listen(3000);
+
+//Setting for view for ejs files
+app.set("view engine", "ejs");
+
+//Directory for EJS files
+app.set('views', path.join(__dirname,'Front-End', 'Views'));
+
 //route and response
 app.get("/", (request, response) => {
-    response.sendFile("./Front-End/index.html", { root: __dirname });
+    response.render("index", { title: "Mytinerator Home"});
 });
 app.get("/budget", (request, response) => {
-    response.sendFile("./Front-End/budget.html", { root: __dirname });
+    response.render("budget", { title: "Mytinerator budget page"});
 });
 //redirect
 app.get("/budgeting", (request, response) => {
-    response.redirect("/budget");
+    response.render("budget", { title: "Mytinerator budget page"});
+});
+app.get("/accommodation", (request, response) => {
+    response.render("accommodation", { title: "Mytinerator accommodation page"});
 });
 //404 page
 app.use((request, response) => {
-    response.status(404).sendFile("./Front-End/404.html", { root: __dirname });
+    response.render("404", { title: "Mytinerator 404 page"});
 });
-
-
 
 //test terminal connection
 //const sMessage = "Hello world";
