164c5798b99b11db42f41e9e636d3c0e588e12c9
Shaneburk3 <92030458+Shaneburk3@users.noreply.github.com>
2024-05-21 17:23:08 +0100

    JS files for each page
    edited app.js for directing ejs head files. Each route is given a specific JS file.

--- app.js
+++ app.js
@@ -11,27 +11,29 @@ app.listen(3000);
 app.set("view engine", "ejs");
 
 //Directory for EJS files
-app.set('views', path.join(__dirname,'front-end', 'Views'));
+app.set('views', path.join(__dirname,'Front-End', 'Views'));
 app.use(express.static(path.join(__dirname,'front-end')));
-app.use('/js', express.static(path.join(__dirname,'back-end')));
+
+//Directory for Back-End files
+app.use(express.static(path.join(__dirname, 'Back-End')));
 
 //route and response
 app.get("/", (request, response) => {
-    response.render("index", { title: "Mytinerator Home"});
+    response.render("index", { title: "Mytinerator Home", script: ['/JS/index.js']});
 });
 app.get("/budget", (request, response) => {
-    response.render("Budget", { title: "Mytinerator Budget Page"});
+    response.render("Budget", { title: "Mytinerator Budget Page", script: ['/JS/budgetCal.js']});
 });
 //redirect
 app.get("/budgeting", (request, response) => {
-    response.render("Budget", { title: "Mytinerator Budget page"});
+    response.render("Budget", { title: "Mytinerator Budget page", script: ['/JS/budgetCal.js']});
 });
 app.get("/accommodation", (request, response) => {
-    response.render("accommodation", { title: "Mytinerator Accommodation page"});
+    response.render("accommodation", { title: "Mytinerator Accommodation", script: ['/JS/accomm.js']});
 });
 //404 page
 app.use((request, response) => {
-    response.render("404", { title: "Mytinerator 404 page"});
+    response.render("404", { title: "Mytinerator 404 page", script: ['']});
 });
 
 //test terminal connection
