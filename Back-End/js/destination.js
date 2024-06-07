d7ed5db182cfd57794d5e54e8a61d785bf4ef7e1
Shaneburk3 <92030458+Shaneburk3@users.noreply.github.com>
2024-06-07 09:46:37 +0100

    edited weather api images
    added images to weather folder

--- Back-End/js/destination.js
+++ Back-End/js/destination.js
@@ -22,6 +22,7 @@ async function loadweather() {
         console.log(data);
       
         var cityCard = document.getElementById('place').innerHTML = "";
+        var overviewCard = document.getElementById('sky').innerHTML= "Overview: ";
         var humidCard = document.getElementById('humidity').innerHTML= "Humidity: ";
         var tempCard = document.getElementById('temp').innerHTML= "Temperature: ";
         var feelsCard = document.getElementById('feels').innerHTML= "Feels like: ";
@@ -31,6 +32,28 @@ async function loadweather() {
       
         var CityData = data["name"];
         document.getElementById('place').innerHTML += CityData;
+
+        var skyOverlook = data["weather"][0]["description"];
+        document.getElementById('sky').innerHTML += skyOverlook;
+
+        if(skyOverlook.includes("clouds")){
+          document.getElementById("weatherImg").src = "/images/weather/Clouds.png";
+        } else if (skyOverlook.includes("snow")) {
+          document.getElementById("weatherImg").src = "/images/weather/Snow.png";
+        } else if (skyOverlook.includes("rain")) {
+          document.getElementById("weatherImg").src = "/images/weather/Rain.png";
+        } else if (skyOverlook.includes("drizzle")) {
+          document.getElementById("weatherImg").src = "/images/weather/Drizzle.png";
+        } else if (skyOverlook.includes("clear")) {
+          document.getElementById("weatherImg").src = "/images/weather/Clear.png";
+        }else if (skyOverlook.includes("thunderstorm")) {
+          document.getElementById("weatherImg").src = "/images/weather/Thunderstorm.png";
+        }else if (skyOverlook.includes("mist") ||skyOverlook.includes("fog") ||skyOverlook.includes("haze") ||skyOverlook.includes("sand")) {
+          document.getElementById("weatherImg").src = "/images/weather/Atmosphere.png";
+        }else {
+          document.getElementById("weatherImg").src = "/images/weather/Base.png";
+        }
+      
       
         var humidityData = data["main"]["humidity"];
         document.getElementById('humidity').innerHTML += humidityData;
@@ -47,7 +70,7 @@ async function loadweather() {
         var tempMin = Math.floor(data["main"]["temp_min"] - 273.15);
       document.getElementById('tempMin').innerHTML += tempMin;
       
-        var windSpeed = data["wind"]["speed"];
+        var windSpeed = Math.round(data["wind"]["speed"] * 3.59) + ' km/h';
         document.getElementById('wind').innerHTML += windSpeed;
 
       }      
